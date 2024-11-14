18919291d227ebd88d00a19703ec183104bfeb04
Henrik Sandklef <hesa@sandklef.com>
2024-11-15 00:04:05 +0100

    add default trigger

--- tests/python/dummy_main.py
+++ tests/python/dummy_main.py
@@ -1,6 +1,7 @@
 import json
 import sys
 
+from licomp.interface import ObligationTrigger
 from licomp.main_base import LicompParser
 from test_lcm import DummyLicense
 
@@ -8,7 +9,8 @@ def main():
     o_parser = LicompParser(DummyLicense(),
                             name = "Dummy license impl",
                             description = "Just a dummy impl",
-                            epilog = "Do not use")
+                            epilog = "Do not use",
+                            default_trigger = ObligationTrigger.BIN_DIST)
     o_parser.run()
     
 if __name__ == '__main__':
