799a9587ad14fddea1112c421c11f1411d492fb6
hesa <hesa@localhost>
2024-11-18 22:06:49 +0000

    Release 0.1.6

--- licomp/config.py
+++ licomp/config.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-licomp_version = '0.1.5'
+licomp_version = '0.1.6'
 licomp_module_name = 'licomp'
 licomp_description = 'An interface to generalise license compatibility resources'
 licomp_epilog = ''
