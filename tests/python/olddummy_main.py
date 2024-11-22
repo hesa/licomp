9a74112cff4fcc40859949f28cd522ff4f58e825
Henrik Sandklef <hesa@sandklef.com>
2024-11-22 17:41:08 +0100

    add test for subclass using old api

---
+++ tests/python/olddummy_main.py
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: 2024 Henrik Sandklef
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import json
+import sys
+
+from licomp.interface import Provisioning
+from licomp.interface import UseCase
+from licomp.main_base import LicompParser
+from licomp_olddummy import OldDummyLicense
+
+def main():
+    o_parser = LicompParser(OldDummyLicense(),
+                            name = "Dummy license impl",
+                            description = "Just a dummy impl",
+                            epilog = "Do not use",
+                            default_usecase = UseCase.LIBRARY,
+                            default_provisioning = Provisioning.BIN_DIST)
+    o_parser.run()
+    
+if __name__ == '__main__':
+    main()
+
+    
